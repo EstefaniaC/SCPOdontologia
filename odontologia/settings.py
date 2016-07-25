0fae75d6eeedb25fe0392ed82253b0d1268a8376
EstefaniaC <estefaniacervantes91@gmail.com>
2016-07-25 12:59:45 -0500

    cambio de nombre de la base de datos

--- odontologia/settings.py
+++ odontologia/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'odontologia.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'odontologia',
+        'NAME': 'SCPOdontologia',
         'USER': 'postgres',
         'PASSWORD': 'estefanii91',
         'HOST': 'localhost',
